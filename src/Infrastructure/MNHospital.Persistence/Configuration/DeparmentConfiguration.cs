7caedcaa7e0a4dff9c668ad2c9d1cc2c0cefd4a3
Javadov Eldar <eldarej@code.local>
2024-03-15 11:53:02 +0400

    MH-5 Created Context

---
+++ src/Infrastructure/MNHospital.Persistence/Configuration/DeparmentConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MNHospital.Domain.Entities;
+
+namespace MNHospital.Persistence.Configuration;
+
+public class DeparmentConfiguration : IEntityTypeConfiguration<Department>
+{
+    public void Configure(EntityTypeBuilder<Department> builder)
+    {
+        builder.Property(d => d.Name)
+            .IsRequired()
+            .HasMaxLength(255);
+    }
+}
